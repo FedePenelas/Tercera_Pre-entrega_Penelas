c9c308d796ca06f1533f904cf1f9fb717efd9227
FedePenelas <fpenelasdeveloper@gmail.com>
2023-07-02 01:14:44 -0300

    Decimo commit Proyecto Final

--- AdoptAR/AdoptAR_login/urls.py
+++ AdoptAR/AdoptAR_login/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('cerrar_sesion/', views.Logout.as_view(), name='cerrar_sesion'),
     path('perfil/', views.mostrar_perfil, name='mostrar_perfil'),
     path('perfil/editar/', views.editar_perfil, name='editar_perfil'),
-    path('perfil/cambiar-password/', views.CambiarPassword.as_view(), name='cambiar_contraseña'),
+    path('perfil/cambiar_contraseña/', views.CambiarPassword.as_view(), name='cambiar_contraseña'),
     path('perfil/eliminar/<int:pk>/', views.EliminarPerfil.as_view(), name='eliminar_perfil'),
 ]
